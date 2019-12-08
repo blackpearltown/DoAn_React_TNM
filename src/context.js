ab118e9991519fe994714ff98cb8476361f3b425
blackpearltown <tranngocminh16294@gmail.com>
2019-12-08 14:52:14 +0700

    third commit

--- src/context.js
+++ src/context.js
@@ -9,7 +9,7 @@ class ProductProvider extends Component {
     state = {
         products: [],
         detailProduct: detailProduct,
-        cart: storeProducts,
+        cart: [],
         modelOpen: false,
         modelProduct: detailProduct,
         cartSubTotal: 0,
@@ -43,6 +43,7 @@ class ProductProvider extends Component {
             return {detailProduct: product}
         })
     };
+
     addToCart = id => {
         let tempProducts = [...this.state.products];
         const index = tempProducts.indexOf(this.getItem(id));
@@ -52,32 +53,106 @@ class ProductProvider extends Component {
         const price = product.price;
         product.total = price;
         this.setState(() => {
-            return {products: tempProducts, cart: [...this.state.cart, product]};
-        })
+                return {products: tempProducts, cart: [...this.state.cart, product]};
+            },
+            () => {
+                this.addTotals();
+            })
     };
     openModel = id => {
         const product = this.getItem(id);
-        this.setState(() =>{
+        this.setState(() => {
             return {modelProduct: product, modelOpen: true}
         })
     };
-    closeModel = () =>{
-        this.setState(()=> {
+
+    closeModel = () => {
+        this.setState(() => {
             return {modelOpen: false}
         })
     };
 
-    increment =(id) => {
-        console.log('this is increment method')
+    increment = (id) => {
+        let tempCart = [...this.state.cart];
+        const selectedProduct = tempCart.find(item => item.id === id);
+        const index = tempCart.indexOf(selectedProduct);
+        const product = tempCart[index];
+
+        product.count = product.count + 1;
+        product.total = product.count * product.price;
+
+        this.setState(() =>{
+            return{cart: [...tempCart]}
+        }, () =>{
+            this.addTotals();
+        })
     };
-    decrement =(id) => {
-        console.log('this is decrement method')
+
+    decrement = (id) => {
+        let tempCart = [...this.state.cart];
+        const selectedProduct = tempCart.find(item => item.id === id);
+        const index = tempCart.indexOf(selectedProduct);
+        const product = tempCart[index];
+
+        product.count = product.count - 1;
+        if(product.count === 0){
+            this.removeItem(id)
+        }else{
+            product.total = product.count * product.price;
+
+            this.setState(() =>{
+                return{cart: [...tempCart]}
+            }, () =>{
+                this.addTotals();
+            })
+        }
+
+
     };
-    removeItem =(id) =>{
-        console.log('item removed')
+
+    removeItem = (id) => {
+        let tempProducts = [...this.state.products];
+        let tempCart = [...this.state.cart];
+        tempCart = tempCart.filter(item => item.id !== id);
+        const index = tempProducts.indexOf(this.getItem(id));
+        let removedProduct = tempProducts[index];
+        removedProduct.inCart = false;
+        removedProduct.count = 0;
+        removedProduct.total = 0;
+
+        this.setState(() =>{
+            return{
+                cart: [...tempCart],
+                products: [...tempProducts]
+            }
+        }, () =>{
+            this.addTotals();
+        })
+
     };
-    clearCart = () =>{
-        console.log('cart was cleared')
+
+    clearCart = () => {
+        this.setState(() => {
+            return {cart: []}
+        }, () => {
+            this.setProducts();
+            this.addTotals()
+        });
+    };
+
+    addTotals = () => {
+        let subTotal = 0;
+        this.state.cart.map(item => (subTotal += item.total));
+        const tempTax = subTotal * 0.1;
+        const tax = parseFloat(tempTax.toFixed(2));
+        const total = subTotal + tax;
+        this.setState(() => {
+            return {
+                cartSubTotal: subTotal,
+                cartTax: tax,
+                cartTotal: total,
+            };
+        });
     };
 
     render() {
